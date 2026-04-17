a297260006eaae728350612b873e506d24db862e
Aman Pandey <amanpandey30558@gmail.com>
2026-04-17 16:59:01 +0530

    Explicitly define JavaMailSender bean to fix Render AutoConfiguration UnsatisfiedDependencyException

--- src/main/java/com/engineerDigest/journalApp/JournalApplication.java
+++ src/main/java/com/engineerDigest/journalApp/JournalApplication.java
@@ -10,6 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -28,6 +31,22 @@ public class JournalApplication {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("himanshupandey916132@gmail.com");
+        mailSender.setPassword("juto shsx tiun hetm");
+        
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        
+        return mailSender;
     }
 
 }
